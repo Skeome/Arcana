2b4db7d59eb9587c00ecaf1e6bf8db8656931bf4
Skeome <skeome@gmail.com>
2025-11-08 04:38:14 -0800

    First 10 Cards

--- app/src/main/java/com/github/skeome/arcana/MainActivity.kt
+++ app/src/main/java/com/github/skeome/arcana/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.github.skeome.arcana
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,58 +16,65 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // --- Create Instances of Our Cards ---
-        // We are using the 'SpiritCard' and 'SpellCard' blueprints
-        // from your 'Card.kt' file.
+        // --- 1. Create Our Card Instances ---
+        val fireSpirit = SpiritCard("Fire Spirit", "Fire", 2, 3, 2, "A basic spirit of flame.")
+        val waterElemental = SpiritCard("Water Elemental", "Water", 4, 3, 5, "A sturdy elemental.")
+        val fireball = SpellCard("Fireball", "Fire", 3, "Deal 4 damage to any target.")
+        val earthGolem = SpiritCard("Earth Golem", "Earth", 5, 4, 6, "Has Guard.")
+        val heal = SpellCard("Heal", "Water", 1, "Restore 3 health to a Spirit.")
+        val windScout = SpiritCard("Wind Scout", "Wind", 1, 1, 1, "Draw a card.")
 
-        val fireSpirit = SpiritCard(
-            name = "Fire Spirit",
-            element = "Fire",
-            aetherCost = 2,
-            power = 3,
-            health = 2,
-            rulesText = "A basic spirit of flame."
+        // --- 2. Create Our Collections ---
+
+        // A 'listOf' is a read-only list. This is our "Master Deck" blueprint.
+        val masterDeckList = listOf(
+            fireSpirit, fireSpirit, // We can add multiple copies
+            waterElemental,
+            fireball, fireball,
+            earthGolem,
+            heal,
+            windScout, windScout
         )
 
-        val waterElemental = SpiritCard(
-            name = "Water Elemental",
-            element = "Water",
-            aetherCost = 4,
-            power = 3,
-            health = 5,
-            rulesText = "A sturdy elemental of the deep."
-        )
+        // A 'mutableListOf' is a list we can change. This is the player's "Deck".
+        // We create it by copying the master list.
+        val playerDeck: MutableList<Any> = masterDeckList.toMutableList()
 
-        val fireball = SpellCard(
-            name = "Fireball",
-            element = "Fire",
-            aetherCost = 3,
-            rulesText = "Deal 4 damage to any target."
-        )
+        // This is where our game logic would live.
+        // '.shuffle()' is a built-in function for lists!
+        playerDeck.shuffle()
+
+        // 'mutableListOf' is also perfect for a hand, starting empty.
+        val playerHand: MutableList<Any> = mutableListOf()
 
 
-        // --- Why this is awesome ---
+        // --- 3. Simulate the Game! ---
+        println("--- GAME START ---")
+        println("Player's deck has ${playerDeck.size} cards.") // .size gives the count
+        println("Shuffling deck...")
 
-        // 1. Data classes give you a 'toString()' for free!
-        //    Just print the object to see all its data.
-        println("--- My New Cards ---")
-        println(fireSpirit)
-        println(waterElemental)
-        println(fireball)
+        // Let's simulate drawing our 7-card starting hand
+        println("\n--- DRAWING HAND ---")
+        repeat(7) {
+            // '.removeFirst()' takes the top card (index 0) and returns it.
+            // This is exactly like drawing from a real deck!
+            val drawnCard = playerDeck.removeFirst()
 
-        // 2. You can access individual properties using '.'
-        println("\n--- Using The Data ---")
-        var wizardHP = 100
-        println("Wizard HP: $wizardHP")
-        println("Casting 'Fireball' costs ${fireball.aetherCost} Aether...")
-        println("It deals 4 damage!")
-        wizardHP = wizardHP - 4 // We'd use our function here later
-        println("Wizard HP is now: $wizardHP")
+            // '.add()' puts the card into our hand
+            playerHand.add(drawnCard)
+        }
+
+        println("\n--- Player's Hand ---")
+        // We can loop through a list
+        playerHand.forEach { card ->
+            println(card) // This will print the data class info
+        }
+
+        println("\n--- Deck After Draw ---")
+        println("Player's deck now has ${playerDeck.size} cards.")
 
 
-        // --- This is all the default UI code ---
-        // It just displays "Hello Android!" on the screen.
-        // We'll replace this later with our game.
+        // --- Default UI Code ---
         setContent {
             ArcanaTheme {
                 Surface(
@@ -80,7 +88,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// This is just a 'Composable' (UI) function for the default screen
+// ... (The Greeting and GreetingPreview functions at the bottom remain unchanged) ...
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -89,7 +97,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-// This is just for the 'Preview' window in Android Studio
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
